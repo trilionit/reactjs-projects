334e91a40e40d5391f01a98589f7d63cd49282c3
trilionit <pokax2000@gmail.com>
2017-01-29 12:59:24 -0500

    Basic Router TUtorial

--- react-router/App.js
+++ react-router/App.js
@@ -1,5 +1,13 @@
-import React {Component} from 'react';
-
+import React, {Component} from 'react';
+import Nav from './Nav';
 class App extends Component{
-	
+	render(){
+		return(
+			<div className="App">
+			<Nav />
+			{this.props.children}
+			</div>
+		)
+	}	
 }
+export default App;
--- react-router/index.js
+++ react-router/index.js
@@ -1,3 +1,21 @@
 import React from 'react';
-import { render } from 'react-dom';
-import { Router, Route, Link, browserHistory } from 'react-router';
+import ReactDom from 'react-dom';
+import { Router, Route, IndexRoute, hashHistory} from 'react-router';
+
+import App from './App';
+import Home from './react-router/Home';
+import About from './react-router/About';
+
+
+ReactDom.render(
+	<Router history={hashHistory}>
+		<Route path="/" component={App}>
+			<IndexRoute component={Home} />
+			<Route path="/" component={Home} />
+			<Route path="/about" component={About} />
+		</Route>
+	</Router>,
+
+	document.getElementById('root')
+);
+
